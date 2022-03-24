bfa549d7d1e1495a9bd8fd86fcbe053cbaba683b
Daniil Kremyansky <densca@mail.ru>
2022-03-25 00:32:12 +0700

    Add rating views unit test

--- my_site/rating/tests.py
+++ my_site/rating/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+
+
+class ViewsTestCase(TestCase):
+    def test_url_rating_exists(self):
+        response = self.client.get('/rating/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_rating_accessible_by_name(self):
+        response = self.client.get(reverse('rating'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_rating_view_uses_correct_template(self):
+        response = self.client.get(reverse('rating'))
+        self.assertTemplateUsed(response, 'rating/index.html')
+
+    def test_url_create_rating_exists(self):
+        response = self.client.get('/rating/new')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_create_rating_accessible_by_name(self):
+        response = self.client.get(reverse('create_rating'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_rating_view_uses_correct_template(self):
+        response = self.client.get(reverse('create_rating'))
+        self.assertTemplateUsed(response, 'rating/create_rating.html')
+
+    def test_url_detail_rating_exists(self):
+        response = self.client.get('/rating/testman')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_detail_rating_accessible_by_name(self):
+        response = self.client.get(reverse('detail_rating', kwargs={'user': "testman"}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_rating_view_uses_correct_template(self):
+        response = self.client.get(reverse('detail_rating', kwargs={'user': "testman"}))
+        self.assertTemplateUsed(response, 'rating/details_view.html')
