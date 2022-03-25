1103067f199b253966dff60a154902c7b1fb9504
Daniil Kremyansky <densca@mail.ru>
2022-03-25 13:54:13 +0700

    Add max and min values for rating grade

--- my_site/rating/models.py
+++ my_site/rating/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from main.models import CustomUser
@@ -6,4 +7,4 @@ from main.models import CustomUser
 class Rating(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     comment = models.TextField()
-    grade = models.IntegerField()
+    grade = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
