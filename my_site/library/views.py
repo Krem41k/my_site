79a29bceec49c777af06e59546aef1f9333efcb3
Daniil Kremyansky <densca@mail.ru>
2022-04-02 14:38:19 +0700

    Add views for library

--- my_site/library/views.py
+++ my_site/library/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+
+from .models import Document
+
+
+class DocumentListView(ListView):
+    model = Document
+    template_name = 'library/index.html'
+    context_object_name = 'documents'
 
 
 class DocumentCreateView(CreateView):
+    model = Document
     template_name = 'library/create_document.html'
     success_url = reverse_lazy('library')
