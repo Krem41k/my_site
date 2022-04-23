b36280101e563773781c7c86026f129e3b0d7d87
Daniil Kremyansky <densca@mail.ru>
2022-04-23 18:05:15 +0700

    Add search by author, title in library app

--- my_site/library/urls.py
+++ my_site/library/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import DocumentCreateView, DocumentListView
+from .views import DocumentCreateView, DocumentListView, SearchResultsView
 
 urlpatterns = [
     path('', DocumentListView.as_view(), name='library'),
     path('new/', DocumentCreateView.as_view(), name='create_document'),
+    path('search/', SearchResultsView.as_view(), name='search_document_results'),
 ]
--- my_site/library/views.py
+++ my_site/library/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 
@@ -15,3 +16,15 @@ class DocumentCreateView(CreateView):
     form_class = DocumentForm
     template_name = 'library/create_document.html'
     success_url = reverse_lazy('library')
+
+
+class SearchResultsView(ListView):
+    model = Document
+    template_name = 'library/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = Document.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query) | Q(description__icontains=query)
+        )
+        return object_list
